5d893ae88cdd44bd36e9fd686ac8041bc5a1b6c0
Javier Evans <evansjavier16@gmail.com>
2019-06-12 19:50:07 +0000

    assetsPublicPath (build) corregido

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../www/dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dist/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
