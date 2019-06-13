a8e185ab4544068996161f3c6da655aaecc2b78d
Javier Evans <evansjavier16@gmail.com>
2019-06-13 16:02:39 +0000

    Agregado JS de Cordova

--- src/main.js
+++ src/main.js
@@ -50,3 +50,30 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+/* CORDOVA APP */
+    var app = {
+        // Application Constructor
+        initialize: function() {
+            document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        },
+    
+        // deviceready Event Handler
+        //
+        // Bind any cordova events here. Common events are:
+        // 'pause', 'resume', etc.
+        onDeviceReady: function() {
+            this.receivedEvent('deviceready');
+            alert("Device Ready");
+        },
+    
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            console.log('Received Event: ' + id);
+        }
+    };
+    
+    app.initialize();
+    
+/* END - CORDOVA APP */
