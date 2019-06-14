7e7e88062fbe518bdb6abfc10e72be053356a370
Javier Evans <evansjavier16@gmail.com>
2019-06-14 17:53:13 +0000

    Removido cordova-plugin-console

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -242,7 +242,6 @@ module.exports.metadata =
     "cordova-plugin-network-information": "1.3.4",
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-statusbar": "2.1.3",
-    "cordova-plugin-console": "1.0.7",
     "cordova-plugin-file": "6.0.1"
 }
 // BOTTOM OF METADATA
